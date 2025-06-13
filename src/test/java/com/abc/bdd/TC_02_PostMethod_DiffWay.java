19fc975d23132255b03b564622842d6994541464
AdhikariChumki <chumkia17@gmail.com>
2025-06-13 16:19:44 +0530

    POST REQ in different way

--- src/test/java/com/abc/bdd/TC_02_PostMethod_DiffWay.java
+++ src/test/java/com/abc/bdd/TC_02_PostMethod_DiffWay.java
@@ -1,8 +1,12 @@
 package com.abc.bdd;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -41,18 +45,7 @@ public class TC_02_PostMethod_DiffWay {
 				.log().all();
 	}
 
-	//Delete student record
-		@Test(priority=2)
-	  void testDelete() {
-		  
-		  RestAssured.
-		  given()
-		  .when()
-		  .delete("http://localhost:3000/students/4")
-		  .then()
-		  .statusCode(200); 
-		  
-	  }
+	
 		
 		//USING JsON
 		void testPostusingjSON() {
@@ -82,10 +75,71 @@ public class TC_02_PostMethod_DiffWay {
 		}
 
 		//USING POJO CLASS
+	
+		void testPostUsingPOJO() {
+			
+			Pojo_PostReq data = new Pojo_PostReq();//create obj of our pojo class
+			data.setName("Chumki");
+			data.setName("India");
+			data.setPhone("895214752");
+			
+			String courseArr [] = {"java", "C"};
+			data.setCourses(courseArr);
+			
+			RestAssured
+					.given()
+					.contentType("Application/json")
+					.body(data) //converting json obj to tostring , 
+											//because if data in the string formnat it will be covert into json format
+					.when()
+					.post("http://localhost:3000/students")
+					.then()
+					.statusCode(201)
+					.header("contentType", "Application/json; charset=uft-8")
+					//.body("Name",equalTo("Siliguri"));//for verify
+					.log().all();
+		}
+	
+		//USING EXTARNAL JSON FILE
+		void testPostUsingExtarnalJsonFile() throws FileNotFoundException {
+			
+			File f=new File(".\\body.json");
+			FileReader fr=new FileReader(f); //OENING THE FILE
+			
+			JSONTokener jt =new JSONTokener(fr);//use to extract the data in json format
+			JSONObject data = new JSONObject(jt);
+			
+			
+			RestAssured
+					.given()
+					.contentType("Application/json")
+					.body(data) //converting json obj to tostring , 
+											//because if data in the string formnat it will be covert into json format
+					.when()
+					.post("http://localhost:3000/students")
+					.then()
+					.statusCode(201)
+					.header("contentType", "Application/json; charset=uft-8")
+					//.body("Name",equalTo("Siliguri"));//for verify
+					.log().all();
+		}
+	
 		
-		//test
-	
-	
-	
+		
+		
+		
+		
+		//Delete student record
+				@Test(priority=2)
+			  void testDelete() {
+				  
+				  RestAssured.
+				  given()
+				  .when()
+				  .delete("http://localhost:3000/students/4")
+				  .then()
+				  .statusCode(200); 
+				  
+			  }
 	
 }
